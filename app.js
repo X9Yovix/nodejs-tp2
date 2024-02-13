89efbe06238041b2b3c42aef5a61e730034a6b0e
Karim Ouazzou <karimouazzou@gmail.com>
2024-02-13 15:40:04 +0100

    add car

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const carsRouter = require('./routes/cars');
+
 const app = express();
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/cars', carsRouter);
--- routes/cars.js
+++ routes/cars.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const cars = [
+cars = [
     { id: 1, name: "clio" },
     { id: 2, name: "megane" },
     { id: 3, name: "range" }
-]
+];
 
 router.get('/add', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/../view/cars/add.html`));
+});
+
+router.post('/add', (req, res) => {
+    var id = cars.length + 1;
+    var name = req.body.name;
+
+    var newElement = { id: id, name: name };
+
+    cars.push(newElement);
+    
     res.sendFile(path.join(`${__dirname}/../view/cars/add.html`));
 });
 
